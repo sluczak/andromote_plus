d6994bc88600fa9060143f10708eadaecf10c95d
fester3000 <fester3000@gmail.com>
2016-08-08 23:56:14 +0200

    Confirmed functionality of encoders and engines

--- src/andromote.js
+++ src/andromote.js
@@ -1,69 +1,67 @@
-var rpio = require('rpio');
+var pigpio = require('pigpio'),
+    Gpio = pigpio.Gpio,
+    Notifier = pigpio.Notifier,
+    m1_pwm,
+    m1_A,
+    m1_B,
+    m2_pwm,
+    m2_A,
+    m2_B,
+// enc1_notifier,
+    enc_1,
+    dutyCycle = 100;
 
-// var OnOff = require('onoff').Gpio,
-//     input = new OnOff(24, 'in', 'both');
+const numberOfTicksPerRound = 82;
+const perimeterMM = 188;
+var ENC_GPIO = 16;
 
-var options = {
-    gpiomem: false,
-    mapping: 'physical'
-};
+var distanceCM = 100;
 
-// input.watch(function (err, value) {
-//     if (err) {
-//         console.log("err");
-//         throw err;
-//     }
-//
-//     console.log("zmiana " + value);
-// });
+pigpio.configureClock(5, pigpio.CLOCK_PCM);
+
+m1_A = new Gpio(17, {mode: Gpio.OUTPUT});
+m1_B = new Gpio(2, {mode: Gpio.OUTPUT});
+m1_pwm = new Gpio(13, {mode: Gpio.OUTPUT});
+
+m2_A = new Gpio(27, {mode: Gpio.OUTPUT});
+m2_B = new Gpio(3, {mode: Gpio.OUTPUT});
+m2_pwm = new Gpio(12, {mode: Gpio.OUTPUT});
+
+var tickCount = 0;
+
+enc_1 = new Gpio(ENC_GPIO,
+    {
+        mode: Gpio.INPUT,
+        alert: true
+    });
+
+const limit = distanceCM * 10 / perimeterMM * numberOfTicksPerRound;
+
+(function () {
+    enc_1.on('alert', function (level, tick) {
+        if(level == 1) {
+            tickCount++;
+        }
+
+        if(tickCount >= limit) {
+            m1_A.digitalWrite(1);
+            m1_B.digitalWrite(0);
+            m1_pwm.pwmWrite(0);
+
+            m2_A.digitalWrite(0);
+            m2_B.digitalWrite(1);
+            m2_pwm.pwmWrite(0);
+            console.log(tickCount);
+            console.log(limit)
+        }
+    });
+}());
 
 
-console.log("init");
-rpio.init(options);
+m1_A.digitalWrite(0);
+m1_B.digitalWrite(1);
+m1_pwm.pwmWrite(dutyCycle);
 
-rpio.open(11, rpio.OUTPUT, rpio.HIGH);
-rpio.open(3, rpio.OUTPUT, rpio.LOW);
-
-rpio.open(13, rpio.OUTPUT, rpio.LOW);
-rpio.open(5, rpio.OUTPUT, rpio.HIGH);
-console.log("open");
-// rpio.open(24, rpio.INPUT, rpio.PULL_DOWN);
-// rpio.poll(24, readValues);
-
-rpio.open(33, rpio.PWM);
-rpio.open(32, rpio.PWM);
-//
-rpio.pwmSetClockDivider(4);
-//
-rpio.pwmSetRange(33, 100);
-rpio.pwmSetRange(32, 100);
-//
-rpio.pwmSetData(33, 60);
-rpio.pwmSetData(32, 60);
-
-// function readValues() {
-//     console.log("val: " + rpio.read(24));
-// }
-
-
-
-console.log("PWM");
-rpio.sleep(2);
-console.log("Change");
-rpio.open(11, rpio.OUTPUT, rpio.LOW);
-rpio.open(3, rpio.OUTPUT, rpio.HIGH);
-
-rpio.sleep(2);
-console.log("Close");
-
-rpio.close(11);
-rpio.close(3);
-rpio.close(33);
-rpio.close(13);
-rpio.close(5);
-rpio.close(32);
-// rpio.close(26);
-
-// process.on('SIGINT', function () {
-//     input.unexport();
-// });
+m2_A.digitalWrite(1);
+m2_B.digitalWrite(0);
+m2_pwm.pwmWrite(dutyCycle);
