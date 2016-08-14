a565802955fe90e8912751407af7853ae5900395
fester3000 <fester3000@gmail.com>
2016-08-14 22:36:22 +0200

    Added 'features' library model and sequential tasks execution

--- src/devices/wheel_encoder.js
+++ src/devices/wheel_encoder.js
@@ -4,11 +4,11 @@ var eventEmitter = require('../core/common_event_emitter');
 
 function WheelEncoder(identifier) {
     WheelEncoder.prototype.TICKS_PER_ROUND = 82;
-
+    this.currentTick = 0;
     console.log("Encoder " + identifier + " ready to be attached");
     this.id = identifier;
 
-    WheelEncoder.prototype.attach = function(encoderPinout) {
+    WheelEncoder.prototype.attach = function attach(encoderPinout) {
         console.log("Attaching Encoder " + this.id + " to GPIO -=");
         console.log(encoderPinout);
         const alertedInput = {mode: Gpio.INPUT, alert: true};
@@ -16,16 +16,19 @@ function WheelEncoder(identifier) {
         this.signalB = new Gpio(encoderPinout.pinB, alertedInput);
 
         var self = this;
-        (function () {
-            var tickCount = 0;
+        (function listenToWheelMovement() {
             const eventId = self.id + '_tick';
             self.signalA.on('alert', function (level, tick) {
                 if(level == 1) {
-                    tickCount++;
-                    eventEmitter.emit(eventId, tickCount);
+                    self.currentTick++;
+                    eventEmitter.emit(eventId, self.currentTick);
                 }
             });
         }());
+    };
+
+    WheelEncoder.prototype.getCurrentTick = function getCurrentTick() {
+        return this.currentTick;
     }
 }
 
---
+++ src/features/features.js
@@ -0,0 +1,20 @@
+var _ = require('lodash');
+
+function Features() {
+    this.features = [];
+
+    Features.prototype.load = function loadFeatures(features) {
+        var self = this;
+        _.forEach(features, function(entry) {
+            self.features.push(entry);
+        });
+    };
+
+    Features.prototype.get = function get(featureName) {
+        var feat = _.find(this.features, {'name': featureName}).feature.create();
+        return feat;
+    };
+}
+
+var features = new Features();
+module.exports = features;
